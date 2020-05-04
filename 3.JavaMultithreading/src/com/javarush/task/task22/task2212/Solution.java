2ef21ddde5ce315b684248b9305f64ea6c18863a
elena <elenavw@gmail.com>
2020-05-04 14:10:43 +0400

    till MVS 8

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -1,20 +1,14 @@
 package com.javarush.task.task22.task2212;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-/*
+/* 
 Проверка номера телефона
 */
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
-        Pattern p = Pattern.compile("^\\+\\d+[(]{0,1}\\d{3}[)]{0,1}\\d+[-]{0,1}\\d+[-]{0,1}\\d+$");
-        Matcher m = p.matcher(telNumber);
-        return m.find();
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkTelNumber("+38(050)1234567"));
-
+        System.out.println("потом решу");
     }
 }
