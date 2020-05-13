823a2b4b48db9c6b794b9ba16d63f72c5aafff0c
elena <elenavw@gmail.com>
2020-05-13 16:37:05 +0400

    till 2509

--- 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -1,7 +1,37 @@
 package com.javarush.task.task25.task2508;
 
-public class TaskManipulator implements Runnable {
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    private String threadName;
+    private Thread thread;
+
     @Override
     public void run() {
+        while (!thread.isInterrupted()) {
+            System.out.println(thread.getName());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        thread =  new Thread(this, threadName);
+        this.threadName = threadName;
+        thread.start();
+
+    }
+
+    @Override
+    public void stop() {
+        try {
+
+            this.thread.interrupt();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
     }
 }
