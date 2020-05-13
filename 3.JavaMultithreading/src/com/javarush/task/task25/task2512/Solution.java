69c2a8d1dad3a7c7fa41c7ebe1c670689ecff059
elena <elenavw@gmail.com>
2020-05-13 17:57:03 +0400

    till Refactoring

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2512/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task25.task2512;
+
+/* 
+Живем своим умом
+*/
+public class Solution implements Thread.UncaughtExceptionHandler {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.uncaughtException(Thread.currentThread(), new Exception("ABC", new RuntimeException("DEF", new IllegalAccessException("GHI"))));
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        t.interrupt();
+        recursion(e);
+    }
+
+    public void recursion ( Throwable e) {
+        if (e.getCause() != null) recursion(e.getCause());
+        System.out.println(e.getClass().getName() + ": " + e.getMessage());
+    }
+}
