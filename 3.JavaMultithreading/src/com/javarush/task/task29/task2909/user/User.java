2e1fec6245f8ba34e52e3dd344f20dbfd7efefd3
elena <elenavw@gmail.com>
2020-05-16 15:03:55 +0400

    end Refactoring

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -1,13 +1,12 @@
 package com.javarush.task.task29.task2909.user;
 
 public class User {
+    private Address address;
     private String name;
     private String surname;
     private int age;
 
-    private String country;
-    private String city;
-    private House house;
+    private boolean man;
 
     private Work work;
 
@@ -15,6 +14,14 @@ public class User {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
     }
 
     public void printInfo() {
@@ -54,23 +61,23 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
 
     public Work getWork() {
@@ -79,6 +86,10 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public String getBoss () {
+        return getWork().getBoss();
     }
 
 }
