b532362f55778ded5ce5165ca483c00433e215dc
elena <elenavw@gmail.com>
2020-05-17 20:58:36 +0400

    chat (6) Server done

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,31 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        System.out.println("Введите номер порта сервера:");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Сервер запущен.");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+                continue;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private static class Handler extends Thread {
+        private final Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
