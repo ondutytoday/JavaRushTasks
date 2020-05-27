dc0e9524240dd1f42fec91f057e77931785b390a
elena <elenavw@gmail.com>
2020-05-27 13:07:54 +0400

    Archiver 2 done ZipFileManager.createZip done

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,11 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -9,7 +14,22 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip (Path source) throws Exception{
+    public void createZip(Path source) throws Exception {
+        try (ZipOutputStream outputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+             InputStream inputStream = Files.newInputStream(source);) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            outputStream.putNextEntry(zipEntry);
+           // while (inputStream.available() > 0) {
+                byte [] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                outputStream.write(buffer);
+        //    }
+
+            outputStream.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
