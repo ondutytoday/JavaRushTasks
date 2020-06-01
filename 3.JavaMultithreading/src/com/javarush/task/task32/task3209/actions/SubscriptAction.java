692e35e641b8dcd77f53b5db05843a4f5db06f58
elena <elenavw@gmail.com>
2020-06-01 23:01:17 +0400

    3209 HTML editor 8 + actions package

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task32.task3209.actions;
+
+import javax.swing.text.StyledEditorKit;
+import java.awt.event.ActionEvent;
+
+public class SubscriptAction extends StyledEditorKit.StyledTextAction {
+
+
+    /**
+     * Creates a new StyledTextAction from a string action name.
+     *
+     * @param nm the name of the action
+     */
+    public SubscriptAction() {
+        super("SubscriptAction");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
