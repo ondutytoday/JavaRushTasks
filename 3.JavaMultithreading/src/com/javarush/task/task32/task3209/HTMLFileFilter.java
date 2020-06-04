f6e4c669765fccb39524a260cce2e3a4fd438af8
elena <elenavw@gmail.com>
2020-06-04 12:51:50 +0400

    HTML editor 21 HTMLFileFilter

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/HTMLFileFilter.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+/*        boolean accept = false;
+        if (f.isDirectory()) {
+            accept = true;
+        } else if (f.isFile()) {
+            if (f.getName().toLowerCase().endsWith(".html")
+            || f.getName().toLowerCase().endsWith(".htm")) {
+                accept = true;
+            }
+        }*/
+        return f.isDirectory()
+                || f.getName().toLowerCase().endsWith(".html")
+                || f.getName().toLowerCase().endsWith(".htm");
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы" ;
+    }
+}
