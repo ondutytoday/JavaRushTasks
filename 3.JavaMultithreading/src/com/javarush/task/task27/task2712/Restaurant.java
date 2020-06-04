ce6e5229654ca9f1a0d982f9b7acfeb848628b1e
elena <elenavw@gmail.com>
2020-06-04 19:01:29 +0400

    Restaurant 4

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -13,9 +14,12 @@ public class Restaurant {
 //        tablet.createOrder();
 //        tablet.createOrder();
         Cook cookVanya = new Cook("Vanya");
+        Waiter waiter = new Waiter();
         Tablet tablet = new Tablet(1);
         tablet.addObserver(cookVanya);
+        cookVanya.addObserver(waiter);
         tablet.createOrder();
+
 
     }
 }
