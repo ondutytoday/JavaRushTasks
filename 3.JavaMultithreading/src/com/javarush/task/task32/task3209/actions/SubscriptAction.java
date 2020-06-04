93f706486b8b2e4525b1cccf2fef8349755eea4e
elena <elenavw@gmail.com>
2020-06-04 09:38:15 +0400

    HTML editor 12 SubscriptAction SuperscriptAction

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/actions/SubscriptAction.java
@@ -1,22 +1,23 @@
 package com.javarush.task.task32.task3209.actions;
 
-import javax.swing.text.StyledEditorKit;
+import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.event.ActionEvent;
 
 public class SubscriptAction extends StyledEditorKit.StyledTextAction {
 
-
-    /**
-     * Creates a new StyledTextAction from a string action name.
-     *
-     * @param nm the name of the action
-     */
     public SubscriptAction() {
-        super("SubscriptAction");
+        super(StyleConstants.Subscript.toString());
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent actionEvent) {
+        JEditorPane editor = getEditor(actionEvent);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setSubscript(simpleAttributeSet, !StyleConstants.isSubscript(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
