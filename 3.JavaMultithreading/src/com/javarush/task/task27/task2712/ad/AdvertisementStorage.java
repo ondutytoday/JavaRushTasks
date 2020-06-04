32cd02764f526e41ca816a7afa15c704650a3c18
elena <elenavw@gmail.com>
2020-06-04 19:49:14 +0400

    Restaurant 6

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task27.task2712.ad;
+
+public class AdvertisementStorage {
+    private static AdvertisementStorage instance;
+
+    private AdvertisementStorage () {}
+
+    public static AdvertisementStorage getInstance() {
+        if (instance == null) {
+            instance = new AdvertisementStorage();
+        }
+        return instance;
+    }
+
+}
