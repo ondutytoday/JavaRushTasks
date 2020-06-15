719f8c71048babdc96883b2c2b78d39e129e5a4d
elena <elenavw@gmail.com>
2020-06-15 13:56:24 +0400

    3102

--- 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -1,18 +1,31 @@
 package com.javarush.task.task31.task3102;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 /* 
 Находим все файлы
 */
 public class Solution {
-    public static List<String> getFileTree(String root) throws IOException {
-        return null;
 
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> list = new ArrayList<>();
+        File fileRoot = new File(root);
+        Queue<File> queue = new PriorityQueue<>();
+        Collections.addAll(queue, fileRoot.listFiles());
+        while (!queue.isEmpty()) {
+            File currentFile = queue.poll();
+            if (currentFile.isDirectory()) {
+                Collections.addAll(queue, currentFile.listFiles());
+            } else {
+                list.add(currentFile.getAbsolutePath());
+            }
+        }
+        return list;
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
