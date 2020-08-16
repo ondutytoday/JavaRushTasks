f16ac76d55e667a10561b0fcbe38d2048c803e1f
elena <elenavw@gmail.com>
2020-08-16 13:00:16 +0400

    3202

--- 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
@@ -1,10 +1,17 @@
 package com.javarush.task.task32.task3201;
 
-/* 
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/*
 Запись в существующий файл
 */
 public class Solution {
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
+        RandomAccessFile file = new RandomAccessFile(args [0], "rw");
+        file.seek(Long.parseLong(args[1]));
+        file.writeBytes(args[2]);
 
     }
 }
